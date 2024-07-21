52ebb15b6d382af769a3d5dc65dc736ce06e6199
MINAS\User <ics20072@uom.edu.gr>
2024-07-21 17:46:56 +0300

    Initial scaffolding of the back-end of the app using spring boot.

---
+++ back-end/quality-gates-impact-analyzer/src/main/java/com/uom/quality_gates_impact_analyzer/QualityGatesImpactAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.uom.quality_gates_impact_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityGatesImpactAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityGatesImpactAnalyzerApplication.class, args);
+	}
+
+}
---
+++ back-end/quality-gates-impact-analyzer/src/test/java/com/uom/quality_gates_impact_analyzer/QualityGatesImpactAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uom.quality_gates_impact_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityGatesImpactAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
